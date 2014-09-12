5dc39eab29feb477ae305a9f919502eb9a260278
Alvaro Gonzalez <alvaro@code.cl>
2014-09-12 18:36:06 -0300

    Modificado modulo Facebook
    - Modulo Facebook ahora es publico
    - Se cambia el icono de la app
    - Se elimino el registro de puntaje en clay.io

--- www/js/app.js
+++ www/js/app.js
@@ -38,217 +38,210 @@ var status			= {
 	};
 
 /**
- * Definicion de modulos
+ * Definicion del modulo
  */
-define(['facebook'], function()
-{
+var app = {
 	/**
-	 * Define el modulo principal
+	 * Inicializa el SDK de Facebook
 	 */
-	var app = {
-		/**
-		 * Inicializa el SDK de Facebook
-		 */
-		init			: function()
+	init			: function()
+	{
+		this.watch();
+		FB.init(
 		{
-			this.watch();
-			FB.init(
+			appId				: '1434579543483455',
+			//nativeInterface		: CDV.FB,
+			useCachedDialogs	: false,
+			status				: false
+		});
+		status.init			= 1;
+	},
+
+	/**
+	 * Bindea eventos de facebook
+	 */
+	bind			: function()
+	{
+		FB.Event.subscribe('auth.statusChange', function()
+		{
+			status.event.statusChange		= 1;
+		});
+		FB.Event.subscribe('auth.login', function()
+		{
+			status.event.login				= 1;
+		});
+		FB.Event.subscribe('auth.logout', function()
+		{
+			status.event.logout				= 1;
+		});
+	},
+
+	/**
+	 * Login Facebook
+	 */
+	login			: function(scope)
+	{
+		status.loginPending			= 1;
+		FB._nativeInterface.login(function(response)
+		{
+			if ( response.authResponse )
 			{
-				appId				: '1434579543483455',
-				//nativeInterface		: CDV.FB,
-				useCachedDialogs	: false,
-				status				: false
-			});
-			status.init			= 1;
+				status.logged			= 1;
+				this.info();
+			}
+			else
+			{
+				status.loginCancel		= 1;
+			}
+			status.loginPending		= 0;
 		},
-
-		/**
-		 * Bindea eventos de facebook
-		 */
-		bind			: function()
 		{
-			FB.Event.subscribe('auth.statusChange', function()
-			{
-				status.event.statusChange		= 1;
-			});
-			FB.Event.subscribe('auth.login', function()
-			{
-				status.event.login				= 1;
-			});
-			FB.Event.subscribe('auth.logout', function()
-			{
-				status.event.logout				= 1;
-			});
+			scope: scope || 'email'
 		},
-
-		/**
-		 * Login Facebook
-		 */
-		login			: function(scope)
+		function(e)
 		{
-			status.loginPending			= 1;
-			FB._nativeInterface.login(function(response)
+			status.loginCancel			= 1;
+			status.loginPending			= 0;
+		});
+	},
+
+	/**
+	 * Obtiene el estado de conexion
+	 */
+	loginStatus		: function()
+	{
+		FB.getLoginStatus(function(response)
+		{
+			if ( response.status === 'connected' )
 			{
-				if ( response.authResponse )
-				{
-					status.logged			= 1;
-					this.info();
-				}
-				else
-				{
-					status.loginCancel		= 1;
-				}
-				status.loginPending		= 0;
-			},
+				status.logged			= 1;
+				this.info();
+			}
+			else if ( response.status === 'not_authorized' )
 			{
-				scope: scope || 'email'
-			},
-			function(e)
+				status.logged			= 0;
+			}
+			else
 			{
-				status.loginCancel			= 1;
-				status.loginPending			= 0;
-			});
+				status.logged			= 0;
+			}
+		}, true);
+	},
+
+	/**
+	 * Obtiene la informacion del usuario
+	 */
+	info			: function()
+	{
+		FB.api('/me',
+		{
+			fields		: 'id, name, first_name, last_name, email'
 		},
-
-		/**
-		 * Obtiene el estado de conexion
-		 */
-		loginStatus		: function()
+		function(response)
 		{
-			FB.getLoginStatus(function(response)
+			if ( ! response || response.error )
 			{
-				if ( response.status === 'connected' )
-				{
-					status.logged			= 1;
-					this.info();
-				}
-				else if ( response.status === 'not_authorized' )
-				{
-					status.logged			= 0;
-				}
-				else
-				{
-					status.logged			= 0;
-				}
-			}, true);
-		},
+				status.user.error			= 1;
+			}
+			else
+			{
+				status.user.error			= 0;
+				status.user.id				= response.id;
+				status.user.name			= response.name;
+				status.user.first_name		= response.first_name;
+				status.user.last_name		= response.last_name;
+				status.user.email			= response.email;
+			}
 
-		/**
-		 * Obtiene la informacion del usuario
-		 */
-		info			: function()
+			// TODO: guardar usuario
+		});
+	},
+
+	/**
+	 * Desconecta al usuario de la app
+	 */
+	logout			: function()
+	{
+		status.loggedOut		= 0;
+		FB.logout(function()
 		{
-			FB.api('/me',
-			{
-				fields		: 'id, name, first_name, last_name, email'
-			},
-			function(response)
-			{
-				if ( ! response || response.error )
-				{
-					status.user.error			= 1;
-				}
-				else
-				{
-					status.user.error			= 0;
-					status.user.id				= response.id;
-					status.user.name			= response.name;
-					status.user.first_name		= response.first_name;
-					status.user.last_name		= response.last_name;
-					status.user.email			= response.email;
-				}
+			status.loggedOut		= 1;
+		});
+	},
 
-				// TODO: guardar usuario
-			});
-		},
-
-		/**
-		 * Desconecta al usuario de la app
-		 */
-		logout			: function()
+	/**
+	 * Compartir
+	 */
+	share			: function(name, description)
+	{
+		FB._nativeInterface.dialog(
 		{
-			status.loggedOut		= 0;
-			FB.logout(function()
-			{
-				status.loggedOut		= 1;
-			});
-		},
-
-		/**
-		 * Compartir
-		 */
-		share			: function(name, description)
+			method			: 'feed',
+			name			: name || '-',
+			caption			: '-',
+			description		: description || '-',
+			link			: 'http://google.cl',
+			picture			: 'http://placehold.it/200x200'
+		});
+		/*
+		,function(response)
 		{
-			FB._nativeInterface.dialog(
+			if ( ! response || response.error )
 			{
-				method			: 'feed',
-				name			: name || '-',
-				caption			: '-',
-				description		: description || '-',
-				link			: 'http://google.cl',
-				picture			: 'http://placehold.it/200x200'
-			});
-			/*
-			,function(response)
+				app.debug('Error al obetener datos de usuario');
+			}
+			else
 			{
-				if ( ! response || response.error )
-				{
-					app.debug('Error al obetener datos de usuario');
-				}
-				else
-				{
-					app.debug('Share OK');
-				}
-			},
-			function()
-			{
-				app.debug('Error al compartir.');
-			});
-			*/
-		},
-
-		/**
-		 * Observa cambios
-		 */
-		watch			: function()
-		{
-			watch(status, function()
-			{
-				app.storage(status, 'status');
-			});
-		},
-
-		/**
-		 * Setea el estado en localStorage
-		 */
-		storage			: function(obj, name)
-		{
-			for ( var x in obj )
-			{
-				if ( typeof(obj[x]) === 'object' )
-				{
-					this.storage(obj[x], 'status.' + x);
-				}
-				else
-				{
-					if ( obj.hasOwnProperty(x) )
-					{
-						localStorage.setItem(name + '.' + x, obj[x]);
-					}
-				}
+				app.debug('Share OK');
 			}
 		},
-
-		/**
-		 * Postea el puntaje del usuario al finalizar el juego
-		 */
-		puntaje			: function(tiempo)
+		function()
 		{
-			// TODO: guardar puntaje
-			tiempo		= tiempo || 0;
+			app.debug('Error al compartir.');
+		});
+		*/
+	},
+
+	/**
+	 * Observa cambios
+	 */
+	watch			: function()
+	{
+		watch(status, function()
+		{
+			app.storage(status, 'status');
+		});
+	},
+
+	/**
+	 * Setea el estado en localStorage
+	 */
+	storage			: function(obj, name)
+	{
+		for ( var x in obj )
+		{
+			if ( typeof(obj[x]) === 'object' )
+			{
+				this.storage(obj[x], 'status.' + x);
+			}
+			else
+			{
+				if ( obj.hasOwnProperty(x) )
+				{
+					localStorage.setItem(name + '.' + x, obj[x]);
+				}
+			}
 		}
-	};
-	return app;
-});
+	},
+
+	/**
+	 * Postea el puntaje del usuario al finalizar el juego
+	 */
+	puntaje			: function(tiempo)
+	{
+		// TODO: guardar puntaje
+		tiempo		= tiempo || 0;
+	}
+};
 
 //]]>
